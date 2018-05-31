9f572f45333765f5745f4101b4311fb0ba6b5c9c
WSudul <wojciech.sudul@gmail.com>
2018-05-31 23:57:04 +0200

    export raport to file

--- src/main/report/Report.java
+++ src/main/report/Report.java
@@ -2,11 +2,12 @@ package report;
 
 import berger.BergerCode;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Report {
 
@@ -16,6 +17,7 @@ public class Report {
 
     public Report() {
         timestamp = Instant.now();
+        recordMap = new HashMap<>();
     }
 
     public boolean addRecord(BergerCode originalData, Record record) {
@@ -23,6 +25,19 @@ public class Report {
             return recordMap.get(originalData).add(record);
         } else
             return true;
+    }
+
+    public boolean exportToFile(String fileName) {
+        Path file = Paths.get(fileName + "_" + timestamp.toString());
+        try {
+            Files.createFile(file);
+            Files.write(file, this.toString().getBytes());
+
+        } catch (IOException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+            return false;
+        }
+        return true;
     }
 
     @Override
