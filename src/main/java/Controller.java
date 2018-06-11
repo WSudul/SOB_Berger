ae459b13d0deee9bea9445a215357b9bfd7bbec1
Tomasz Malinowski <tmalinowski247@gmail.com>
2018-06-12 01:56:37 +0200

    Added static Bits representation in GUI

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,21 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class Controller {
-}
+/*
+    @FXML
+    private Button myButton;
+
+    @FXML
+    private Label outputTextArea;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        myButton.setOnAction((event) -> {
+            // Button was clicked, do something...
+            outputTextArea.appendText("Button Action\n");
+        });    }
+*/}
