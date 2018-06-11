1bddc7df19f368e81bec2a3dee5006f3aba7384c
Tomasz Malinowski <tmalinowski247@gmail.com>
2018-06-12 00:53:34 +0200

    Adding FXML's hello world

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //TODO change acces way to FXML file
+        FXMLLoader loader = new FXMLLoader(new File("D:\\Programming\\SOB_Berger\\src\\main\\java\\sample.fxml").toURI().toURL());
+        Parent root = loader.load();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 800, 450));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
