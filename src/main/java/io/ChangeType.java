09d49c9ee29c0edd1255165e0f45d25bcebc3fa3
WSudul <wojciech.sudul@gmail.com>
2018-06-12 20:29:12 +0200

    change type update for id fields

--- src/main/java/io/ChangeType.java
+++ src/main/java/io/ChangeType.java
@@ -1,14 +1,50 @@
 package io;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum ChangeType {
 
-    NONE,
-    SINGLE_ZERO,
-    SINGLE_ONE,
-    MULTIPLE_ZEROS,
-    MULTIPLE_ONES,
-    MULTIPLE_MIXED,
-    MULTIPLE_MIXED_EVEN
+    NONE(0),
+    SINGLE_ZERO(1),
+    SINGLE_ONE(2),
+    MULTIPLE_ZEROS(3),
+    MULTIPLE_ONES(4),
+    MULTIPLE_MIXED(5,false);
 
+    ChangeType(int id){
+        this.id=id;
+        this.detectable=true;
+    }
+
+    ChangeType(int id,boolean detectable){
+        this.id=id;
+        this.detectable=detectable;
+    }
+
+    private static final Map<Integer,ChangeType> map;
+    static {
+        map = new HashMap<>();
+        for (ChangeType type : ChangeType.values()) {
+            map.put(type.id, type);
+        }
+    }
+    public static ChangeType findByKey(int i) {
+        return map.get(i);
+    }
+
+
+    private final int id;
+    private final boolean detectable;
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isDetectable() {
+        return detectable;
+    }
 
 }
+
+
