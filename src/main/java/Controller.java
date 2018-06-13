0b8f101aa9118f8cbcdf282d6206af2c27c0a4fc
Tomasz Malinowski <tmalinowski247@gmail.com>
2018-06-13 20:56:44 +0200

    Changed storing current example to Berger Code from Code Word, changed way of displaying codeword and added Berger code representation

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,18 +2,14 @@ import berger.BergerCode;
 import berger.BitContainerInterface;
 import berger.CheckBits;
 import berger.CodeWord;
-
-import java.net.URL;
-import java.util.*;
-
 import io.ChangeType;
 import io.DataInput;
 import io.DataReader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import report.Record;
@@ -21,25 +17,26 @@ import report.Report;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.*;
 import java.util.function.Function;
 
 
-public class Controller implements Initializable {
+public class Controller {
 
 
     private List<BergerCode> bergerCodes=new ArrayList<>();
-    private static char[] currentExample;
-    private static List<DataInput> dataInputs;
+    private static BergerCode currentExampleBerger;
     private static int indexOfExamples;
+
     public Controller(){
-        dataInputs = null;
-        indexOfExamples = 0;
-        currentExample = null;
+        indexOfExamples = 1;
     }
     private Random generator = new Random(); //temporary - will be updated with random seed
 
     @FXML
     private ArrayList<Button> buttons;
+
     @FXML
     private Button LoadFile, preset1,preset2,preset3,preset4, PreviousExample, NextExample, GenerateRaport;
 
@@ -47,15 +44,11 @@ public class Controller implements Initializable {
     private Label Output, ExamplesCount;
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        Output.setText("Button Action\n");
-    }
+    private HBox hBox1, hBox2;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        PreviousExample.setVisible(false);
-        NextExample.setVisible(false);
-        ExamplesCount.setVisible(false);
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("Button clicked ");
     }
 
     @FXML
@@ -67,7 +60,7 @@ public class Controller implements Initializable {
         for(BergerCode bergerCode: bergerCodes)
         {
             int randomNumber = generator.nextInt(kMax)+kMin;
-
+            System.out.println("randomNumber for type: " + randomNumber);
             BergerCode modifiedInstance=createModifiedInstance(bergerCode,randomNumber);
             ChangeType changeType=ChangeType.findByKey(randomNumber);
             Record record=new Record(modifiedInstance,changeType);
@@ -76,9 +69,7 @@ public class Controller implements Initializable {
         }
         report.exportToFile("SOB_Report");
 
-        }
-
-
+    }
 
     private BergerCode createModifiedInstance(BergerCode bergerCode, int preset){
         BergerCode modifiedInstance = new BergerCode(bergerCode);
@@ -108,8 +99,6 @@ public class Controller implements Initializable {
 
 
     private BergerCode PresetMultipleZeros(BergerCode bergerCode) {
-
-
         CodeWord codeWord = bergerCode.getCodeWord();
         CheckBits checkBits = bergerCode.getCheckBits();
 
@@ -121,7 +110,6 @@ public class Controller implements Initializable {
 
 
     private BergerCode PresetMultipleOnes(BergerCode bergerCode) {
-
         CodeWord codeWord = bergerCode.getCodeWord();
         CheckBits checkBits = bergerCode.getCheckBits();
 
@@ -202,39 +190,102 @@ public class Controller implements Initializable {
         return uniqueIndexes;
     }
 
-
     public void handleButtonActionLoadFromFile(ActionEvent actionEvent) throws IOException {
+        //Chosing File
         FileChooser chooser = new FileChooser();
         chooser.setTitle("Wybierz plik");
         File file = chooser.showOpenDialog(new Stage());
+        List<DataInput> dataInputs;
+
         try {
             dataInputs = DataReader.readJsonData(file.getCanonicalPath());
+
+            //Initializing List of BergerCodes
+            bergerCodes.clear();
+            for (DataInput dataInput : dataInputs) {
+                System.out.println("dataInput: " + dataInput.getType() + " " + dataInput.getData());
+                BergerCode bergerCode = GenerateBergerCode(dataInput);
+                bergerCodes.add(bergerCode);
+            }
+            //Initialize displayed example
+            currentExampleBerger = bergerCodes.get(0);
+
+            //Exposing switch-example Menu
+            ExamplesCount.setText(indexOfExamples + "/" + bergerCodes.size());
+            if(!PreviousExample.isVisible())
+                PreviousExample.setVisible(true);
+            if(!NextExample.isVisible())
+                NextExample.setVisible(true);
+            if(!ExamplesCount.isVisible())
+                ExamplesCount.setVisible(true);
+            System.out.println("" + currentExampleBerger.getCodeWord().toList().toString());
+            //Displaying current example
+            sethBox();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ExamplesCount.setText(indexOfExamples + "/" + (dataInputs.size()-1));
-        PreviousExample.setVisible(true);
-        NextExample.setVisible(true);
-        ExamplesCount.setVisible(true);
-        currentExample = dataInputs.get(indexOfExamples).getData().toCharArray();
-        System.out.println(currentExample);
+    }
+
+    public void sethBox(){
+        hBox1.getChildren().clear();
+        hBox2.getChildren().clear();
+        int numberOfButtons = currentExampleBerger.getCodeWord().toList().size();
+        Button[] buttons = new Button[numberOfButtons];
+        for(int i = 0; i<numberOfButtons;i++){
+            buttons[i] = new Button();
+            buttons[i].setText("" + (currentExampleBerger.getCodeWord().toList().get(i).toString().equals("false")? "0": "1"));
+        }
+        hBox1.getChildren().addAll(buttons);
+
+        numberOfButtons = currentExampleBerger.getCheckBits().toList().size();
+        buttons = new Button[numberOfButtons];
+        for(int i = 0; i<numberOfButtons;i++){
+            buttons[i] = new Button();
+            buttons[i].setText("" + (currentExampleBerger.getCodeWord().toList().get(i).toString().equals("false")? "0": "1"));
+        }
+        hBox2.getChildren().addAll(buttons);
     }
 
     public void handleButtonActionPreviousExample(ActionEvent actionEvent) {
-        if(indexOfExamples > 0) {
-        indexOfExamples--;
-        ExamplesCount.setText(indexOfExamples + "/" + (dataInputs.size()-1));
-        currentExample = dataInputs.get(indexOfExamples).getData().toCharArray();
-        System.out.println(currentExample);
+        if(indexOfExamples > 1) {
+            indexOfExamples--;
+            ExamplesCount.setText(indexOfExamples + "/" + bergerCodes.size());
+            currentExampleBerger = bergerCodes.get(indexOfExamples-1);
+            System.out.println(currentExampleBerger.getCodeWord().toList().toString());
+            sethBox();
         }
     }
 
     public void handleButtonActionNextExample(ActionEvent actionEvent) {
-        if(indexOfExamples < (dataInputs.size()-1)) {
+        if(indexOfExamples < bergerCodes.size()) {
             indexOfExamples++;
-            ExamplesCount.setText(indexOfExamples + "/" + (dataInputs.size()-1));
-            currentExample = dataInputs.get(indexOfExamples).getData().toCharArray();
-            System.out.println(currentExample);
+            ExamplesCount.setText(indexOfExamples + "/" + bergerCodes.size());
+            currentExampleBerger = bergerCodes.get(indexOfExamples-1);
+            System.out.println(currentExampleBerger.getCodeWord().toList().toString());
+            sethBox();
         }
     }
+
+    private BergerCode GenerateBergerCode(DataInput dataInput) {
+        BergerCode bergerCode;
+        switch (dataInput.getType()) {
+            case INT:
+                bergerCode = new BergerCode(Integer.parseInt(dataInput.getData()));
+                break;
+            case LONG:
+                bergerCode = new BergerCode(Long.parseLong(dataInput.getData()));
+                break;
+            case STRING:
+                bergerCode = new BergerCode(dataInput.getData());
+                break;
+            case BYTES:
+                BigInteger bigInteger = new BigInteger(dataInput.getData());
+                bergerCode = new BergerCode(bigInteger.toByteArray());
+                break;
+            default:
+                return null;
+        }
+        return bergerCode;
+    }
+
 }
