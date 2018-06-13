3d7e154183e386c2056849333e573ff7dbd02a5e
WSudul <wojciech.sudul@gmail.com>
2018-06-14 00:12:39 +0200

    fixing missing imports

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,10 +1,4 @@
 import berger.BergerCode;
-import berger.BitContainerInterface;
-import berger.CheckBits;
-import berger.CodeWord;
-
-import java.util.*;
-
 import io.ChangeType;
 import io.DataInput;
 import io.DataReader;
@@ -12,21 +6,24 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import report.Record;
 import report.Report;
+
 import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static presets.Preset.*;
 
 
 public class Controller {
@@ -99,100 +96,7 @@ public class Controller {
     }
 
 
-    private BergerCode PresetMultipleZeros(BergerCode bergerCode) {
 
-
-        CodeWord codeWord = bergerCode.getCodeWord();
-        CheckBits checkBits = bergerCode.getCheckBits();
-
-        SetMultipleOnesToZero(codeWord, generator.nextDouble() / 2.0);
-        SetMultipleOnesToZero(checkBits, generator.nextDouble() / 2.0);
-
-        return bergerCode;
-    }
-
-
-    private BergerCode PresetMultipleOnes(BergerCode bergerCode) {
-
-        CodeWord codeWord = bergerCode.getCodeWord();
-        CheckBits checkBits = bergerCode.getCheckBits();
-
-        SetMultipleZerosToOnes(codeWord, generator.nextDouble() / 2.0);
-        SetMultipleZerosToOnes(checkBits, generator.nextDouble() / 2.0);
-
-        return bergerCode;
-    }
-
-    private void SetMultipleOnesToZero(BitContainerInterface bitContainer, double fraction) {
-        Set<Integer> IndexesOfOnes = GetSpecificIndexes(bitContainer, bit -> (bit));
-        Set<Integer> IndexesToBeCleared = SelectIndexes(IndexesOfOnes, (int) (IndexesOfOnes.size() * fraction));
-        for (Integer index : IndexesToBeCleared) {
-            bitContainer.clearBit(index);
-        }
-    }
-
-    private void SetMultipleZerosToOnes(BitContainerInterface bitContainer, double fraction) {
-        Set<Integer> IndexesOfZeros = GetSpecificIndexes(bitContainer, bit -> (!bit));
-        Set<Integer> IndexesToBeSet = SelectIndexes(IndexesOfZeros, (int) (IndexesOfZeros.size() * fraction));
-        for (Integer index : IndexesToBeSet) {
-            bitContainer.setBit(index);
-        }
-    }
-
-    private Set<Integer> GetSpecificIndexes(BitContainerInterface bitContainer, Function<Boolean, Boolean> function) {
-        Set<Integer> specificIndexes = new TreeSet<>();
-        for (int i = 0; i < bitContainer.length(); ++i) {
-            if (function.apply(bitContainer.getBit(i)))
-                specificIndexes.add(i);
-        }
-        return specificIndexes;
-    }
-
-    private Set<Integer> SelectIndexes(Set<Integer> indexes, int selectSize) {
-        if (selectSize > indexes.size())
-            return null;
-        List<Integer> indexesList = new ArrayList<>(indexes);
-        Set<Integer> selection = new TreeSet<>();
-
-        while (selectSize > 0) {
-            int index = generator.nextInt(indexes.size());
-            selection.add(indexesList.get(index));
-            --selectSize;
-        }
-
-        return selection;
-    }
-
-    private BergerCode PresetMultipleMixed(BergerCode bergerCode) {
-        CodeWord codeWord = bergerCode.getCodeWord();
-        CheckBits checkBits = bergerCode.getCheckBits();
-
-        final int totalLength = codeWord.length() + checkBits.length();
-        final int numberOfBitsToFlip = (totalLength / 10) + generator.nextInt(totalLength / 2);
-
-        Set<Integer> uniqueIndexes = GenerateUniqueIndexes(numberOfBitsToFlip,totalLength);
-
-        final int codeWordMaxIndex = codeWord.length() - 1;
-        final int offset=codeWord.length();
-        for (Integer index : uniqueIndexes) {
-            if (index > codeWordMaxIndex) {
-                checkBits.flipBit(index - offset);
-            } else
-                codeWord.flipBit(index);
-
-        }
-        return bergerCode;
-
-    }
-
-    private Set<Integer> GenerateUniqueIndexes(int n, int maxIndex){
-        Set<Integer> uniqueIndexes =new TreeSet<>();
-        while (uniqueIndexes.size() < n) {
-            int index = generator.nextInt(maxIndex);
-            uniqueIndexes.add(index);
-        }
-        return uniqueIndexes;
-    }
 
     public void handleButtonActionLoadFromFile(ActionEvent actionEvent) throws IOException {
         //Chosing File
